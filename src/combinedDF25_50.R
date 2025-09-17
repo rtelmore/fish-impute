da2cdfb946b957606f2648d2719e828175cf1f69
May Norman <maynorman@Mays-MacBook-Air.local>
2025-09-17 16:24:07 -0500

    h

---
+++ src/combinedDF25_50.R
@@ -0,0 +1,25 @@
+library(plyr)
+library(tab)
+
+
+load("/Users/maynorman/Desktop/fish_impute_git/results_20250903_mar_norm_cor25.RData") 
+resultsdf25 = results_df %>%
+  mutate(corr = .25)
+load("/Users/maynorman/Desktop/fish_impute_git/results_20250829_mar_norm.RData")
+resultsdf50 = results_df %>%
+  mutate(corr = .50)
+
+combin_df = bind_rows(resultsdf25, resultsdf50)
+
+ggplot(combin_df,facet_grid(rows = corr, cols = M))
+combin_df %>% mutate(name = substring(name,1,4)) %>% ggplot(aes(x = as.factor(name), y = -log(value), color = as.factor(M))) + geom_boxplot() + facet_grid(corr~M+method)
+
+medians_tbl <- combin_df %>%
+  mutate(name = substring(name, 1, 4)) %>%
+  group_by(corr, M, method, name) %>%
+  summarise(median_val = median(-log(value), na.rm = TRUE), .groups = "drop")
+
+medians_tbl
+
+medians <- ddply(combin_df,.(TYPE),summarise,med = median(combin_df))
+tabmedians()
