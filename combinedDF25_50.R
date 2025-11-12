0eb142d8c6c3cfc3121f055dbe0e54429d289ba9
May Norman <maynorman@Mays-MacBook-Air.local>
2025-11-12 11:26:10 -0600

    fixed

---
+++ combinedDF25_50.R
@@ -0,0 +1,46 @@
+
+
+library(plyr)
+library(tab)
+
+
+
+load("../fish_impute_git/results_20250903_mar_norm_cor25.RData") 
+resultsdf25 = results_df %>%
+  mutate(corr = .25)
+load("../fish_impute_git/results_20250829_mar_norm.RData")
+resultsdf50 = results_df %>%
+  mutate(corr = .50)
+
+combin_df = bind_rows(resultsdf25, resultsdf50)
+
+ggplot(combin_df,facet_grid(rows = corr, cols = M))
+combin_df %>% mutate(name = substring(name,1,4)) %>% ggplot(aes(x = as.factor(name), y = -log(value), color = as.factor(M))) + geom_boxplot() + facet_grid(corr~M+method)
+
+medians_tbl <- combin_df %>%
+  mutate(name = substring(name, 1, 4)) %>%
+  group_by(corr, M, method, name) %>%
+  summarise(median_val = median(-log(value), na.rm = TRUE), .groups = "drop")
+
+medians_tbl
+
+medians <- ddply(combin_df,.(TYPE),summarise,med = median(combin_df))
+tabmedians()
+
+
+library(plyr)
+library(tab)
+
+
+
+load("/Users/maynorman/Desktop/fish_impute_git/src/results_mar_chisq_0.25.RData") 
+resultsdf25chi = results_df %>%
+  mutate(corr = .25)
+load("/Users/maynorman/Desktop/fish_impute_git/src/results_mar_chisq_0.5.RData")
+resultsdf50chi = results_df %>%
+  mutate(corr = .50)
+
+combin_df = bind_rows(resultsdf25chi, resultsdf50chi)
+
+ggplot(combin_df,facet_grid(rows = corr, cols = M))
+combin_df %>%filter(name %in% c("Original","Missing","Imp1")) %>% mutate(name = substring(name,1,4)) %>% ggplot(aes(x = as.factor(name), y = -log(value), color = as.factor(M))) + geom_boxplot() + facet_grid(corr~n+method)
